046b70e97766ba3921799475ed3cfcd9d55fe135
maxmodrr <maxmodrr@gmail.com>
2024-10-21 14:07:07 +0300

    results

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+const link = document.querySelectorAll('.list-item__link');
+
+link.forEach((elem) => {
+  elem.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const bigImg = document.querySelector('#largeImg');
+  const images = document.querySelectorAll('.gallery__img.gallery__thumb');
+
+  images.forEach((image) => {
+    image.addEventListener('click', () => {
+      const srcImg = image.getAttribute('src');
+      const indexThumb = srcImg.indexOf('-thumb');
+      const newSrc = 'images' + srcImg.slice(0, indexThumb) + '.png';
+      bigImg.setAttribute('src', newSrc);
+    });
+  });
+});
